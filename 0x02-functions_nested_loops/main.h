c34c344e1373e20a4819957e5dcce37a4d63f822
KobiManuel <emmakobi91@gmail.com>
2022-03-15 14:08:22 -0700

    task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+<<<<<<< HEAD
+void jack_bauer(void);
+=======
+int _islower(int c);
+>>>>>>> 1ff3c2c338586ea7c755ea8d7c773538221565a4
